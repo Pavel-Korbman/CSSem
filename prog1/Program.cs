cba62ead7f0443150c005e55a9f17e26c05536fa
Pavel <pavel@b2profit.ru>
2023-10-12 14:11:53 +0300

    Какой то левый файл ./

---
+++ prog1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите квадрат");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num2 == num*num)
+{
+ Console.WriteLine("Yes");
+}
+else
+{
+Console.WriteLine("No");
+}
