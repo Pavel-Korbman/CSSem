0fb5c9348adb0c90aeeb082662785f1a52c47418
Pavel <pavel@b2profit.ru>
2023-10-13 20:51:01 +0300

    запомнили примеры заданий

--- Program.cs
+++ Program.cs
@@ -1,5 +1,28 @@
-﻿Console.WriteLine("Введите число");
-int num = Convert.ToInt32(Console.ReadLine());
+﻿//Console.WriteLine("Введите число");
+//int num = Convert.ToInt32(Console.ReadLine());
 
-int sum = num * num;
-Console.WriteLine(sum);
+//int sum = num * num;
+//Console.WriteLine(sum);
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber > secondNumber)
+{
+   Console.Write("Первое число ");
+   Console.Write(firstNumber);
+   Console.Write(" больше чем второе число ");
+   Console.Write(secondNumber);
+}
+else if(firstNumber < secondNumber)
+{
+ Console.Write("Первое число ");
+   Console.Write(firstNumber);
+   Console.Write(" меньше чем второе число ");
+   Console.Write(secondNumber);   
+}
+else
+{
+Console.WriteLine("Введённые числа равны ");
+Console.Write(firstNumber);
+}
