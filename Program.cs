61050b902128d52935a61dc49e74038bd63151a0
Pavel <pavel@b2profit.ru>
2023-10-13 21:37:42 +0300

    Решение первой задачи

--- Program.cs
+++ Program.cs
@@ -1,23 +1,23 @@
-﻿Console.Write("Введите первое число: ");
+﻿Console.WriteLine("Введите число");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
-if (firstNumber > secondNumber)
+if(firstNumber > secondNumber)
 {
-   Console.Write("Первое число ");
-   Console.Write(firstNumber);
-   Console.Write(" больше чем второе число ");
-   Console.Write(secondNumber);
+Console.Write("Первое число ");
+Console.Write(firstNumber);
+Console.Write(" больше чем второе число ");
+Console.Write(secondNumber);
 }
 else if(firstNumber < secondNumber)
 {
- Console.Write("Первое число ");
-   Console.Write(firstNumber);
-   Console.Write(" меньше чем второе число ");
-   Console.Write(secondNumber);   
+Console.Write("Первое число ");
+Console.Write(firstNumber);
+Console.Write(" меньше чем второе число ");
+Console.Write(secondNumber);   
 }
 else
 {
-Console.WriteLine("Введённые числа равны ");
+Console.Write("Введённые числа равны ");
 Console.Write(firstNumber);
 }
